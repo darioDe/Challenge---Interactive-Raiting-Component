7cc6618a692e3ed24a4d7765fa595e9d9a6b883a
Ruben Duarte <rdduarte1811@gmail.com>
2022-11-18 06:38:20 -0300

    finally commit

--- src/components/SubmitedState.js
+++ src/components/SubmitedState.js
@@ -8,7 +8,7 @@ const SubmitedState = ({number}) => {
     <div className='submited'>
      <div className='cont-submited center' >
         <img className='img-thankyou' src={thankyou} alt="" />
-        <p>You selected {number} out of 5</p>
+        <p className='selected-paragraph'>You selected {number} out of 5</p>
         <h1> Thank you! </h1>
         <p> We appreciate you taking the time to give a rating. If you ever need more support, 
         don't hesitate to get in touch! </p>
